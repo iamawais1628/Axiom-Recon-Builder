a0a4aff913094cd6e2aefa3379044a4e9664d815
Awais Tahir <hafizmuhammadawais831@gmail.com>
2026-05-22 14:41:04 +0500

    Create App component with ReconciliationTool

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReconciliationTool from './pages/ReconciliationTool';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ReconciliationTool />
+    </div>
+  );
+}
+
+export default App;
